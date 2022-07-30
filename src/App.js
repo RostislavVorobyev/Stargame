2ea59bf3828f9068ce7768623c8297f7be31d529
Rostislav Vorobyov <r.vorobyov@godeltech.com>
2022-07-31 02:18:25 +0300

    Base logic for number picks

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 
 function PlayNumber(props) {
   return (
-    <button 
-    className="number" 
-    style={{backgroundColor : colors[props.status]}}
-    onClick={() => alert(props.id)}>{props.id}</button>
+    <button
+      className="number"
+      style={{ backgroundColor: colors[props.status] }}
+      onClick={() => props.onClick(props.id, props.status)}
+      >
+        {props.id}
+      </button>
   );
-} 
+}
 
 function StarDisplay(props) {
   return (
@@ -22,18 +25,40 @@ function StarDisplay(props) {
 function App() {
   const [stars, setStars] = useState(random(1, 9));
   const [candidateNums, setCandidates] = useState([]);
-  const [availableNums, setAvailable] = useState(range(1,9));
+  const [availableNums, setAvailable] = useState(range(1, 9));
 
   const candidatesAreWrong = sum(candidateNums) > stars;
-  
+
   const numberStatus = (number) => {
-  	if (!availableNums.includes(number)) {
-    	return 'used';
+    if (!availableNums.includes(number)) {
+      return 'used';
     }
     if (candidateNums.includes(number)) {
-    	return candidatesAreWrong ? 'wrong': 'candidate';
+      return candidatesAreWrong ? 'wrong' : 'candidate';
     }
     return 'available';
+  };
+
+  const onNumberClick = (number, currentStatus) => {
+    if (currentStatus === 'used') {
+      return;
+    }
+
+		const newCandidateNums =
+      currentStatus === 'available'
+        ? candidateNums.concat(number)
+        : candidateNums.filter(cn => cn !== number);
+
+    if (sum(newCandidateNums) !== stars) {
+      setCandidates(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+      setStars(randomSumIn(newAvailableNums, 9));
+      setAvailable(newAvailableNums);
+      setCandidates([]);
+    }
   };
 
   return (
@@ -47,7 +72,11 @@ function App() {
         </div>
         <div className="right">
           {range(1, 9).map(id =>
-            <PlayNumber status={numberStatus(id)} id={id} />
+            <PlayNumber
+              key={id}
+              onClick={(onNumberClick)}
+              status={numberStatus(id)}
+              id={id} />
           )}
         </div>
       </div>
@@ -68,6 +97,22 @@ const range = (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min 
 const random = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
 
 const sum = (arr) => arr.reduce((acc, curr) => acc + curr, 0);
+
+const randomSumIn= (arr, max) => {
+  const sets = [[]];
+  const sums = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0, len = sets.length; j < len; j++) {
+      const candidateSet = sets[j].concat(arr[i]);
+      const candidateSum = sum(candidateSet);
+      if (candidateSum <= max) {
+        sets.push(candidateSet);
+        sums.push(candidateSum);
+      }
+    }
+  }
+  return sums[random(0, sums.length - 1)];
+}
 
 
 export default App;
